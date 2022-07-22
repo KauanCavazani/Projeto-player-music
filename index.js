1410c7ae977507d3e44eb6b5d7465be0f17e1e60
KauanCavazani <Kauan.cavazani@gmail.com>
2022-07-22 15:34:18 -0300

    add: progressbar, function nextMusic e function prevMusic

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ let currentMusic = 0;
 let isShuffling = false;
 let isMuted = false;
 
+var progressArea = document.getElementById("progress-area");
+var progressBar = document.getElementById("progress-bar");
+var playPrevMusic = document.getElementById("play-prevMusic");
 
 const myMusics = [ 
     {
@@ -32,6 +35,10 @@ function setCurrentMusic(id) {
     nameMusic.innerText = `${myMusics[id].name}`;
     nameArtist.innerText = `${myMusics[id].artist}`;
     musicPlaying.src = `${myMusics[id].song}`;
+
+    progressBar.style.width = '0%';
+
+    
 }
 
 //Toggle de play e pause
@@ -41,35 +48,96 @@ function toggleIsPlaying() {
     if(isPlaying) {
         playMusicIcon.src = './assets/images/icons/play-music.svg';
         isPlaying = false;
-        pauseMusic(currentMusic);
+        pauseMusic(playMusicIcon);
     } else {
         playMusicIcon.src = './assets/images/icons/pause-music.svg';
         isPlaying = true;
-        playMusic(currentMusic);
+        playMusic(playMusicIcon);
     }
 }
 
+var music = document.getElementById("music-playing");
+
 //Começar uma música
-function playMusic(id) {
-    var music = document.getElementById("music-playing");
+function playMusic(icon) {
+    var playMusicIcon = document.getElementById("play-music-icon");
+    playMusicIcon.src = './assets/images/icons/pause-music.svg';
+    isPlaying = true;
+    music = document.getElementById("music-playing");
     music.play();
 }
 
 //Pausar uma música
-function pauseMusic(id) {
-    var music = document.getElementById("music-playing");
+function pauseMusic(icon) {
+    var playMusicIcon = document.getElementById("play-music-icon");
+    playMusicIcon.src = './assets/images/icons/play-music.svg';
+    isPlaying = false;
+    music = document.getElementById("music-playing");
     music.pause();
 }
 
+music.addEventListener("timeupdate", (e) => {
+    var currentTime = e.target.currentTime;
+    var duration = e.target.duration;
+    let progressWidth = (currentTime / duration) * 100;
+    progressBar.style.width = `${progressWidth}%`;
+
+    let musicCurrentTime = document.getElementById("current");
+    let musicDurationTime = document.getElementById("duration");
+
+    music.addEventListener("loadeddata", () => {
+        let audioDuration = music.duration;
+        let totalMin = Math.floor(audioDuration / 60);
+        let totalSec = Math.floor(audioDuration % 60);
+        if(totalSec < 10) {
+            totalSec = `0${totalSec}`;
+        }
+        musicDurationTime.innerText = `${totalMin}:${totalSec}`;
+    });
+
+    let currentMin = Math.floor(currentTime / 60);
+    let currentSec = Math.floor(currentTime % 60);
+    if(currentSec < 10) {
+        currentSec = `0${currentSec}`;
+    };
+    musicCurrentTime.innerText = `${currentMin}:${currentSec}`;
+
+});
+
+progressArea.addEventListener("click", (e) => {
+    let progressWidthVal = progressArea.clientWidth;
+    let clickedOffSetX = e.offsetX;
+    let songDuration = music.duration;
+
+    music.currentTime = ((clickedOffSetX / progressWidthVal) * songDuration);
+})
+
 //Próxima Música
 function nextMusic() {
-
+    if(currentMusic < (myMusics.length - 1)) {
+        currentMusic++;
+        setCurrentMusic(currentMusic);
+        playMusic();  
+    };
 }
 
 //Voltar de música
 function prevMusic() {
-
+    if(currentMusic > 0) {
+        currentMusic--;
+        setCurrentMusic(currentMusic); 
+        playMusic();   
+    }
 }
+
+playPrevMusic.addEventListener("click", () => {
+    setCurrentMusic(currentMusic);
+    playMusic();
+});
+
+playPrevMusic.addEventListener("dblclick", () => {
+    prevMusic();
+})
 
 //Embaralhar
 function shuffling() {
